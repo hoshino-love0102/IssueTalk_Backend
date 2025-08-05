6695628c812e730180a766e9e4e2d1480cc35bc8
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-08-05 23:28:41 +0900

    userId랑 비번 nickname만 받는걸로 수정

--- src/main/java/com/issuetalk/auth/dto/SignupRequestDto.java
+++ src/main/java/com/issuetalk/auth/dto/SignupRequestDto.java
@@ -4,14 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignupRequestDto {
-    private String username;
+    private String userId;
     private String password;
     private String nickname;
-    private LocalDate birthDate;
 }

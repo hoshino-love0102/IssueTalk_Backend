f55f99de2318772e0a3e79f0b9534cfd35f5273b
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-08-06 18:29:22 +0900

    vercel링크 추가

--- src/main/java/com/issuetalk/config/SecurityConfig.java
+++ src/main/java/com/issuetalk/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000/", "https://issuetalk-backend.onrender.com/")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(false);

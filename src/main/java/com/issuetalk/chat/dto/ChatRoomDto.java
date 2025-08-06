ffe82b01208996f4395e10d32f18c69d2f259baa
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-08-06 17:33:36 +0900

    feat: 채팅방 정보를 담는 ChatRoomDto 클래스 추가

--- src/main/java/com/issuetalk/chat/dto/ChatRoomDto.java
+++ src/main/java/com/issuetalk/chat/dto/ChatRoomDto.java
@@ -1,4 +1,16 @@
 package com.issuetalk.chat.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class ChatRoomDto {
+
+    private String roomId; // 채팅방 ID
+    private String topic; // 채팅 주제 (topic ID 또는 이름)
+    private String lastMessage; // 마지막 메시지 내용
+    private LocalDateTime lastUpdated; // 마지막 메시지 시간
 }
